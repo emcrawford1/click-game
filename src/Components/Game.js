0f3c51072eba98f5124c920d556081899e73e303
mrCrawford <noreply>
2019-06-06 20:53:54 -0500

    Added additional code for deployment to GitHub pages.

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -5,12 +5,13 @@ const box = {
     marginRight: '0.75%',
     marginBottom: '0%',
     marginLeft: '0.75%',
-    cursor: 'pointer'
-
+    cursor: 'pointer',
+    hover: 'scale(1.02)'
 }
+
 function Game(props) {
   return (
-    <img className="box rounded-lg" onClick={props.onClick} style={box} src={props.url} alt="picture1"  />
+    <img className="box rounded-lg" onMouseOver="this.style.transform: scale(1.02)" onClick={props.onClick} style={box} src={props.url} alt="picture1"  />
   );
 }
 
